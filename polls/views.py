05b1a80aae3ad75385b9d2231c9207db58fc6fa0
Khusiima Luke Masaba <masabaluke@gmail.com>
2025-02-05 23:46:30 +0300

    change details model and add details template

--- polls/views.py
+++ polls/views.py
@@ -8,8 +8,13 @@ def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
+  
 def detail(request, question_id):
-  return HttpResponse(f"You're looking at question {question_id}")
+  try:
+    question = Question.objects.get(pk=question_id)
+  except Question.DoesNotExist:
+    raise Http404("Question does not exist")
+  return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
   response = "You're looking at the results of question %s."
